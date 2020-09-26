cc11ccc05c0f15b08266c37554539b8b4cb5baa0
Sai Charan <polavarapucharan@hotmail.com>
2020-09-26 16:57:38 +0530

    first change

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import loginSignup from "./components/loginSignup/loginSignup.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>jnfdni</h1>
+    </div>
+  );
+}
+
+export default App;
